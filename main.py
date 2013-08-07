b8fd09438d3f7f4241a5a6202b94497732bc190a
vishen <pentecostjonathan@gmail.com>
2013-08-07 21:53:09 +1000

    moved layout to kv file

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from kivy.app import App
 from kivy.core.window import Window
+from kivy.properties import ObjectProperty
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
 from kivy.uix.screenmanager import SlideTransition, ScreenManager, Screen
 from kivy import utils as kivy_utils
+from kivy.lang import Builder
 
 from ChessBoard import ChessBoard
 
@@ -17,71 +19,58 @@ SQUARES = ["a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8", "a7", "b7", "c7", "d7
 img_piece_abv={"B":"WBishop", "R":"WRook", "N":"WKnight", "Q":"WQueen", "K":"WKing", "P": "WPawn",
 "b":"BBishop", "r":"BRook", "n":"BKnight", "q":"BQueen", "k":"BKing", "p":"BPawn"}
 
+
+
+# Builder.load_file('schess.kv')
+
 class ChessApp(App):
 
+    # chess_grid = ObjectProperty(None)
 
-    def refresh_board(self, show_pieces=False):
+    def refresh_board(self, show_pieces=False, show_piece_coord=True, show_background_color=True):
+
+        dark_color = (0.2, 0.3, 0.8, 0.5)
+        light_color = (0.7, 0.7, 0.6, 1)
  
         # flatten lists into one list of 64 squares
         squares = [item for sublist in self.chessboard.getBoard() for item in sublist]
         print squares
         for i, p in enumerate(squares):
-            sq = self.squares[i]
-            sq.source = sq.background_down
+            light = i % 2 == (i / 8) % 2
+
+            square = self.squares[i]
+
+            if show_piece_coord:
+                square.text = '%s' % SQUARES[i]
+
+            if show_background_color:
+                if light:
+                    square.background_color = light_color
+
+                else:
+                    square.background_color = dark_color
 
 
     def build(self):
-        self.from_move = None
-        self.to_move = None
         self.chessboard = ChessBoard()
         self.squares = []
-        self.use_engine = False
-        self.last_touch_down_move = None
-        self.last_touch_up_move = None
 
-        parent = BoxLayout(size_hint=(1,1))
-        # grid = GridLayout(cols=8, rows=8, padding=10, spacing=0, size=(575,575), size_hint=(0,0))
-        grid = GridLayout(rows=8, padding=10, spacing=0)
+        # Had to manually load due to not being able to 
+        # dynamically add buttons
+        self.load_kv('chess.kv')
+        
+        root = self.root
+        chess_grid = root.ids.chess_grid
 
-        for i, name in enumerate(SQUARES):
-            light = i % 2 == (i / 8) % 2
+        for square in SQUARES:
+            bt = Button()
 
-            background_color = (0.233, 0.166, 0.115, 0.8)
-            background_normal = ''
-
-            if not light:
-                background_color = (1, 0, 0, 1)
-
-            button_kwargs = {
-                'text': '%s' % SQUARES[i],
-                'background_color': background_color,
-                'background_normal': background_normal,
-            }
-            bt = Button(**button_kwargs)
-            
-
-
-            grid.add_widget(bt)
+            chess_grid.add_widget(bt)
             self.squares.append(bt)
-
-        parent.add_widget(grid)
 
         self.refresh_board()
 
-        # if self.is_desktop():
-        #     self._keyboard = Window.request_keyboard(
-        #         self._keyboard_closed, self)
-        #     self._keyboard.bind(on_key_down=self._on_keyboard_down)
-
-        #     self.start_engine_thread()
-
-        sm = ScreenManager()
-        board_screen = Screen(name='main')
-        board_screen.add_widget(parent)
-        sm.add_widget(board_screen)
-
-
-        return sm
+        return root
 
 
 if __name__ == '__main__':
